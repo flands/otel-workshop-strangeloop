86de36afd3d0a4c4a8c63bacbbd33fc70f7da2e8
jkwatson <jwatson@splunk.com>
2021-09-20 13:21:05 -0700

    Add a python service

---
+++ python/app.py
@@ -0,0 +1,25 @@
+# python app.py
+
+from flask import Flask
+from flask import make_response
+import requests
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return 'hello from python\n'
+
+@app.route('/hello/proxy/<path:subpath>')
+def proxy(subpath):
+
+    parts = subpath.split('/')
+    proxyTarget = parts[0]
+    rest = '/'.join(parts[1:])
+
+    url = f'http://{proxyTarget}/hello' if (len(parts) == 1) else f'http://{proxyTarget}/hello/proxy/{rest}'
+    r = requests.get(url)
+    return f'{r.text}hello from python\n'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
